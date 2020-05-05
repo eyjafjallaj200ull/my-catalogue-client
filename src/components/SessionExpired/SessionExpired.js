abb5671af9e58a062eee0d7cb4c973e0a7123a25
neslinur <snsoykan13@gmail.com>
2020-05-05 22:37:57 +0300

    Remove unnecessary imports from SessionExpired

--- src/components/SessionExpired/SessionExpired.js
+++ src/components/SessionExpired/SessionExpired.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { observer } from "mobx-react"
-import {storeContext} from "../../utils/storeContext"
-import useStore from "../../utils/useStore"
 
 const SessionExpired = () => {
     return <div className="text-center session-expired mb-3">Your session has expired. To continue to access your Google Books data, please log out and sign in again.</div>
